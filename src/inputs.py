e823d942d4cde313c35134899cca41641bf0fcbc
naugusti528 <naugusti@ucsc.edu>
2026-09-21 19:50:00 -0700

    Update inputs.py with reference comments for next steps

--- src/inputs.py
+++ src/inputs.py
@@ -1,1 +1,7 @@
+# inputs are x and t
+
+# Latin Hypercube Sampling for generating collocation points on boundary
+# interior points, initial conditions, boundary conditions
+
+# format tensors for automatic differentiation
 
